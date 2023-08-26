1d195a3ac6ef887e2d3ed01dfe92e7fbcc6760ce
Sumer Patel <patelsum@oregonstate.edu>
2023-08-26 13:47:15 -0700

    added placeholder for guessing words

--- src/GuessWord.tsx
+++ src/GuessWord.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+type GuessWordProps = {
+  word: string,
+  guessedLetters: string[],
+  reveal: boolean
+}
 
 const GuessWord = () => {
+  const word = 'test'
+  const guessedLetters = ['t', 'e', 's', 't']
+  const reveal = false
+
   return (
-    <div>
-      
+    <div style={{ display: 'flex', gap: '0.25em', fontSize: '6rem', fontWeight: 'bold', textTransform: 'uppercase', fontFamily: 'monospace' }}>
+      { word.split("").map((letter, index) => (
+        <span style={{ borderBottom: '0.1em black solid'}} key={index}>
+          <span style={{ visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden', }}>{letter}</span>
+        </span>
+      ))}
     </div>
   )
 }
